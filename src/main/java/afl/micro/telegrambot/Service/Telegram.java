fb009e0593040487af6e485e143fcb676e25a7b4
Antonio Ferrer Lopez <buceocv@gmail.com>
2022-08-07 17:33:55 +0200

    Cleaning bot -> StartPoint

--- src/main/java/afl/micro/telegrambot/Service/Telegram.java
+++ src/main/java/afl/micro/telegrambot/Service/Telegram.java
@@ -1,43 +1,52 @@
 package afl.micro.telegrambot.Service;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAnimation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.games.Animation;
 
 @Service
-@Slf4j
 public class Telegram extends TelegramLongPollingBot {
+
+    private final String username = "jasperSpringBot";
+
+    private final String token = "5510373867:AAFcFeM_eZ33PBYq8Z9qgYoZQUgGgHqZ8bE";
+
     public String getBotUsername() {
-        return "jasperSpringBot";
+        return username;
     }
 
     @Override
     public String getBotToken() {
-        return "5510373867:AAFcFeM_eZ33PBYq8Z9qgYoZQUgGgHqZ8bE";
+        return token;
     }
+
 
     @Override
     public void onUpdateReceived(Update update) {
         final String received = update.getMessage().getText();
-        log.info("MENSAJE: " + received);
         final long chatId = update.getMessage().getChatId();
 
         SendMessage response = new SendMessage();
+
+        Animation reveivedImg = update.getMessage().getAnimation();
+
+        SendAnimation sendImg = new SendAnimation();
+
         response.setChatId(String.valueOf(chatId));
 
-        if(received.equals("5")){
-            log.info("ES 5");
+        if (received.equals("5")) {
             response.setText("Ya sabes la rima");
-        }else{
-            response.setText("Respuesta al mensaje DE " + chatId  + " de bot:" + received);
-        }
+        } else {
 
-        try{
-            execute(response);
-        }catch (Exception e){
-            e.printStackTrace();
+            try {
+                execute(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
